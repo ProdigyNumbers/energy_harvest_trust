9a0ec0e11af8547d61cbb5d2f1f80cf33b1bcaba
Sunayana Ghosh <sunayanag@gmail.com>
2022-01-19 08:50:54 +0100

    how to use retrieve images cli

--- apps/retrieve_images.py
+++ apps/retrieve_images.py
@@ -8,7 +8,7 @@ Command line interface for retrieving Sentinel-1 images and saving them to Googl
 Before running this script, you need to have a Google account and a Google Drive account.
 Followed by the following steps:
 `$ earthengine authenticate`
-``
+`$ python apps/retrieve_images.py ./data/input/polygon0-jan.json`
 """
 
 
