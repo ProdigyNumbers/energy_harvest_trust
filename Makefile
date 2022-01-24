8c9d7f402477b061d1b62ef1a085d7e77b59f925
Carlos Mougan <carmougan@gmail.com>
2022-01-24 20:56:04 +0100

    black formatting

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ gitall:
 	git add .
 	@read -p "Enter commit message: " message; 	git commit -m "$$message"
 	git push
+black:
+	python -m black .
---
+++ modeling/exploration.py
@@ -0,0 +1,14 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# Other
+df = pd.read_csv(
+    "../data/output/S1A_IW_GRDH_1SDV_20200105T005145_20200105T005210_030658_03837F_FB13.csv"
+)
+# %%
+
+# %%
+df
+# %%
